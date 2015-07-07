db6a565764aedbe289fc5b270312ffba1a661b9b
Richard Park <objectiv@gmail.com>
2015-07-07 00:38:10 -0400

    Fix vertical spacing of meta-data on permalink page

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-/* v0.1.5 - 2015-07-07T03:00:08.873Z - https://github.com/r-park/tumblr-theme */
+/* v0.1.6 - 2015-07-07T04:36:39.276Z - https://github.com/r-park/tumblr-theme */
 $(function(){$(".posts").imagesReady().then(function(e){e.show();var n=new Quartz({container:e[0],items:".post",columnClass:"g-col",mediaQueries:[{query:"screen and (max-width: 37.49em)",columns:1},{query:"screen and (min-width: 37.5em) and (max-width: 49.99em)",columns:2},{query:"screen and (min-width: 50em)",columns:3}]}),t=infiniteScroll({itemSelector:".post",nextSelector:".pagination__next",threshold:1500,waitForImages:!0});t.on("load:end",function(e,t){n.append(e.items),Tumblr.LikeButton.get_status_by_page(e.page),t()}),t.load()})});
