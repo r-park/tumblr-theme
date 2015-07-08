a6f5fdc16df267c5c176d99d268eeee864cfd2b7
Richard Park <objectiv@gmail.com>
2015-07-08 17:24:47 -0400

    v0.1.10

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-/* v0.1.9 - 2015-07-08T02:53:58.178Z - https://github.com/r-park/tumblr-theme */
+/* v0.1.10 - 2015-07-08T21:24:11.511Z - https://github.com/r-park/tumblr-theme */
 $(function(){$(".posts").imagesReady().then(function(e){e.show();var n=new Quartz({container:e[0],items:".post",columnClass:"g-col",mediaQueries:[{query:"screen and (max-width: 37.49em)",columns:1},{query:"screen and (min-width: 37.5em) and (max-width: 49.99em)",columns:2},{query:"screen and (min-width: 50em)",columns:3}]}),t=infiniteScroll({itemSelector:".post",nextSelector:".pagination__next",threshold:1500,waitForImages:!0});t.on("load:end",function(e,t){n.append(e.items),Tumblr.LikeButton.get_status_by_page(e.page),t()}),t.load()})});
